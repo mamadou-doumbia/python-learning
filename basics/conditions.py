c5d7912fade9619e5bb144746cd139e81d1f432a
Mamadou Doumbia <mamadou.db216@gmail.com>
2026-09-22 19:00:41 +0100

    Update conditions.py

--- basics/conditions.py
+++ basics/conditions.py
@@ -1,1 +1,11 @@
+# Python Basics - Conditions
+# Author: Mamadou Doumbia
 
+note = 5
+
+if note >=10:
+  print("Passed")
+elif note >= 8 and note <10:
+  print("Retake")
+else :
+    print("Failed")
