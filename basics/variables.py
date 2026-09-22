c68fb3aa452b773463b68d2dc6b67af437d2e9dd
Mamadou Doumbia <mamadou.db216@gmail.com>
2026-09-22 18:24:05 +0100

    Update variables.py

--- basics/variables.py
+++ basics/variables.py
@@ -1,15 +1,13 @@
-#Python - Basics - Variables
-#Author: Mamadou Doumbia
+# Python - Basics - Variables
+# Author: Mamadou Doumbia
 
 name = "Mamadou"
-age = 20
 country = "Mali"
 student = True
 university = "Faculty of Science of Bizerte"
 field = "Math - Computer Science"
 
 print("Name:", name)
-print("Age:", age)
 print("Country:", country)
 print("Student:", student)
 print("University:", university)
